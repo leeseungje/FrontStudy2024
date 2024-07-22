4fad95eefb57a1ab00daf5f68ce6780a9f5fe49a
NC-KOREA\leeseungje <leeseungje@ncsoft.com>
2024-07-22 20:58:19 +0900

    chor: 사용 설명 추가

--- Day4/context/src/context/UserContext.tsx
+++ Day4/context/src/context/UserContext.tsx
@@ -30,6 +30,10 @@ export const UserProvider = ({ children }: { children: ReactNode }) => {
   const queryClient = useQueryClient()
   const router = useRouter()
 
+  /**
+   * 사용자를 불러오는 함수
+   * @param id - 불러올 사용자 ID, 없으면 모든 사용자를 불러옴
+   */
   const fetchUser = async (id?: string) => {
     setIsLoading(true)
     setIsError(false)
@@ -43,10 +47,16 @@ export const UserProvider = ({ children }: { children: ReactNode }) => {
     }
   }
 
+  // 컴포넌트가 처음 마운트될 때 모든 사용자 데이터를 불러옴
   useEffect(() => {
     fetchUser()
   }, [])
 
+  /**
+   * 사용자 데이터를 생성, 수정, 삭제하기 위해 React Query의 useMutation 훅을 사용
+   * useMutation 훅을 통해 데이터베이스와 상호작용하며, 성공과 실패 시의 행동을 정의
+   * 유사하게 updateMutation, deleteMutation도 설정
+   */
   const createMutation = useMutation<User, Error, UserParams>({
     mutationFn: createUserFn,
     onSuccess: () => {
@@ -102,6 +112,10 @@ export const UserProvider = ({ children }: { children: ReactNode }) => {
     deleteMutation.mutate(id)
   }
 
+  /**
+   * UserContext.Provider를 사용하여, 하위 컴포넌트들이 UserContext의 값을 사용할 수 있도록 제공
+   * fetchUser, createUser, updateUser, deleteUser 각 함수가 포함 된다.
+   */
   return (
     <UserContext.Provider
       value={{
@@ -123,6 +137,12 @@ export const UserProvider = ({ children }: { children: ReactNode }) => {
   )
 }
 
+/**
+ * useUserContext 훅은 UserContext에서 제공하는 값을 사용하는 데 도움을 주는 커스텀 훅
+ * useContext(UserContext)를 사용하여 현재 Context의 값을 가져옴
+ * UserContext가 제공되지 않는 경우 (즉, UserProvider로 감싸지 않은 컴포넌트에서 사용한 경우), 에러를 발생
+ * UserContextType으로 반환된 Context의 값을 안전하게 사용할 수 있도록 보장
+ */
 export const useUserContext = (): UserContextType => {
   const context = useContext(UserContext)
   if (!context) {
