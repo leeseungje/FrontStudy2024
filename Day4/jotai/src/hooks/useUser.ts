5267bdf36098c33620a96a88816a92bbd6bfe562
NC-KOREA\leeseungje <leeseungje@ncsoft.com>
2024-07-24 13:25:04 +0900

    chore: jotai 오류 해결

--- Day4/jotai/src/hooks/useUser.ts
+++ Day4/jotai/src/hooks/useUser.ts
@@ -1,5 +1,6 @@
 import { atom, useAtom } from 'jotai'
 import { fetcher } from '@/utils/api'
+import { useCallback } from 'react'
 
 // atom을 고유하게 관리하기 위한 Map
 const userAtoms = new Map<string, any>()
@@ -11,21 +12,19 @@ const userAtoms = new Map<string, any>()
 //     return response
 //   })
 
-const usersAtom = atom(async () => {
-  const response = await fetcher('/api/user', { cache: 'no-store' })
-  return response
-})
+const usersAtom = atom<any>(null)
+usersAtom.onMount = (setAtom) => {
+  fetcher('/api/user', { cache: 'no-store' }).then(setAtom)
+}
 
-const userAtom = (id: string) => {
+export const userAtom = (id: string, refetch?: boolean) => {
   // 해당 ID에 대한 Atom이 존재하지 않는 경우에만 새로운 Atom 생성
-  if (!userAtoms.has(id)) {
-    userAtoms.set(
-      id,
-      atom(async () => {
-        const response = await fetcher(`/api/user/${id}`, { cache: 'no-store' })
-        return response
-      }),
-    )
+  if (refetch || !userAtoms.has(id)) {
+    const newAtom = atom<any>(null)
+    newAtom.onMount = (setAtom) => {
+      fetcher(`/api/user/${id}`, { cache: 'no-store' }).then(setAtom)
+    }
+    userAtoms.set(id, newAtom)
   }
   return userAtoms.get(id)
 }
@@ -33,24 +32,29 @@ const userAtom = (id: string) => {
 // useUsers 훅 정의
 export const useUsers = (id?: string) => {
   const [data, setData] = useAtom(id ? userAtom(id) : usersAtom)
-  const isLoading = data === undefined
+  const isLoading = data === null
   const isError = data instanceof Error
 
-  const refreshUsers = async (id?: string) => {
-    try {
-      const response = await fetcher(id ? `/api/user/${id}` : '/api/user', {
-        cache: 'no-store',
-      })
-      setData(response)
-    } catch (error) {
-      setData(error)
-    }
-  }
+  const refreshUsers = useCallback(
+    async (id?: string) => {
+      try {
+        const response = await fetcher(id ? `/api/user/${id}` : '/api/user', {
+          cache: 'no-store'
+        })
+        console.log('response', response)
+        setData(response)
+      } catch (error) {
+        console.log('error', error)
+        setData(error)
+      }
+    },
+    [setData]
+  )
 
   return {
     data,
     isLoading,
     isError,
-    refreshUsers,
+    refreshUsers
   }
 }
